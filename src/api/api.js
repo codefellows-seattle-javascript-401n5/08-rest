b1bb7eec5a08550231cb79ffec635e80002fb966
Aaron <aaronmeade9292@gmail.com>
2018-06-09 13:23:57 -0700

    added some methods

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,12 @@
 
 const router = require('../lib/router.js');
 
+function makeStatusCode(res) {
+    res.statusCode = 200;
+    res.statusMessage = 'good';
+
+}
+
 router.get('/', (req, res) => {
 
     res.statusCode = 200;
@@ -11,12 +17,38 @@ router.get('/', (req, res) => {
     res.end();
 });
 
-router.post('/data', (req, res) => {
+router.post('/api/v1/food', (req, res) => {
 
     res.statusCode = 200;
     res.statusMessage = 'good';
     res.write(JSON.stringify(req.body));
     res.end();
 });
+
+router.put('/api/v1/food', (req, res) => {
+    if(req.url.query.id){
+        makeStatusCode(res);
+        res.write(JSON.stringify(req.body));
+        res.end();
+    } else {
+        res.stausCode = 400;
+        res.statusMessage = 'not good';
+        res.end()
+    }
+});
+
+router.get('/api/v1/food', (req, res) => {
+    if(req.url.query.id){
+        makeStatusCode(res);
+        let meal = req.url.query.id;
+        res.write(`${meal}`);
+        res.end();
+    } else {
+        res.stausCode = 400;
+        res.statusMessage = 'not good';
+        res.end()
+    }
+});
+
 
 module.exports = {};
