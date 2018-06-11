67d1bb63d04783be660125984862faa78f91d2b6
Aaron <aaronmeade9292@gmail.com>
2018-06-11 16:15:40 -0700

    trying to fix linter errors

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -4,31 +4,29 @@ const superagent = require('superagent');
 
 describe('API module', () => {
 
-    it('Should return ID when given one', () => {
+  it('Should return ID when given one', () => {
 
-        return superagent.get('http://localhost:3000/api/v1/food?id=hotdog')
-            .then(response => {
-                expect(response.text).toBe('ID: hotdog was requested')
-            });
-    });
-    
-    it('should respond with not found when an id is not found', () => {
+    return superagent.get('http://localhost:3000/api/v1/food?id=hotdog')
+      .then(response => {
+        expect(response.text).toBe('ID: hotdog was requested');
+      });
+  });
 
-        return superagent.get('http://localhost:3000/api/v1/food?id=badID')
-            .catch(err => {
-                expect(err.responseText).toBe('not found')
-                expect(err.responseStatus).toBe(404)
-            });
-    });
+  it('should respond with not found when an id is not found', () => {
 
-    it('should return with an error status of 400 when a badID is returned', () => {
+    return superagent.get('http://localhost:3000/api/v1/food?id=badID')
+      .catch(err => {
+        expect(err.responseText).toBe('not found')
+        expect(err.responseStatus).toBe(404);
+      });
+  });
 
-        return superagent.get('http://localhost:3000/api/v1/food?id=badID')
-        .catch(err => {
-            expect(err.responseText).toBe('bad request');
-            expect(err.responseStatus).toBe(40)
-        });
-    });
+  it('should return with an error status of 400 when a badID is returned', () => {
 
-    it('should')
-})
+    return superagent.get('http://localhost:3000/api/v1/food?id=badID')
+      .catch(err => {
+        expect(err.responseText).toBe('bad request');
+        expect(err.responseStatus).toBe(40);
+      });
+  });
+});
